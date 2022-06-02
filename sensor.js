12d7ac56cefe2cb9941dc1f7ab60694971c8f652
ninitapliashvili <tafliashvilinini@gmail.com>
2022-06-02 17:54:39 +0400

    fixed sensor

--- sensor.js
+++ sensor.js
@@ -9,13 +9,14 @@ class Sensor {
   }
 
   update() {
+    this.#castRays();
+  }
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      const rayAngle = lerp(
-        this.raySpread / 2,
-        -this.raySpread / 2,
-        i / (this.rayCount - 1)
-      )+this.car.angle;
+      const rayAngle =
+        lerp(this.raySpread / 2, -this.raySpread / 2, i / (this.rayCount - 1)) +
+        this.car.angle;
 
       const start = { x: this.car.x, y: this.car.y };
       const end = {
@@ -24,21 +25,18 @@ class Sensor {
       };
       this.rays.push([start, end]);
     }
-    draw(ctx){
-        for(let i=0; i<this.rayCount;i++){
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle="yellow";
-            ctx.moveTo(
-                this.rays[i][0].x,
-                this.rays[i][0].y
-            );
-            ctx.lineTo(
-                this.rays[i][1].x,
-                this.rays[i][1].y
-            );
-            ctx.stroke();
-        }
+  }
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      if (this.rays.length > 0) {
+        ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+        ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      }
+
+      ctx.stroke();
     }
   }
 }
