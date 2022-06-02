cf22bc5f4338907450a0af5fdf3513ac5e1048e6
ninitapliashvili <tafliashvilinini@gmail.com>
2022-06-02 15:06:45 +0400

    sensor

--- car.js
+++ car.js
@@ -1,4 +1,4 @@
-"use strict";
+"Use strict";
 class Car {
   constructor(x, y, width, height) {
     this.x = x;
@@ -11,11 +11,14 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -64,5 +67,7 @@ class Car {
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
